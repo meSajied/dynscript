dab066e29d8d6508c5ae8c0a235ac1b02572ad67
meSajied <sajiedshahyousuf@gmail.com>
2020-03-06 18:16:19 +0600

    Updated Arithmatics....

---
+++ Arithmetic.js
@@ -0,0 +1,9 @@
+console.log(Math.pow(2, 6), Math.round(.6), Math.ceil(.3))
+
+console.log(Math.floor(.8), Math.abs(-556), Math.max(5, 8, 4), Math.random())
+
+console.log(Math.PI, Math.E, Math.sqrt(3), Math.pow(3, 1/3))
+
+console.log(Math.sin(90), Math.log(123), (Math.log(512)/Math.LN10), (Math.log(512)/Math.LN2))
+
+console.log(Math.exp(3))
