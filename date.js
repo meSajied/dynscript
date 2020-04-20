a6e4a42c3b87b873d40f3ec34e60c17efbe10a19
Sajied Shah Yousuf <sajiedshahyousuf@gmail.com>
2020-04-20 20:26:23 +0600

    Files included...

---
+++ date.js
@@ -0,0 +1,14 @@
+current_time = new Date()
+date_needed = new Date(2010, 13, 5)
+
+current_time.getDate()
+date_needed.getHours()
+date_needed.getUTCHours()
+current_time.getFullYear()
+date_needed.toISOString()
+current_time.toString()
+date_needed.getLocalDate()
+date_needed.toUTCString()
+current_time.toLocaleTimeString()
+
+date_needed.valueOf()
---
+++ float_length.js
@@ -0,0 +1,12 @@
+var n = 123456.789;
+n.toFixed(0);
+n.toFixed(2);
+n.toFixed(5);
+n.toExponential(1);
+n.toExponential(3);
+n.toPrecision(4);
+n.toPrecision(7);
+n.toPrecision(10);
+
+parseInt() // find integer from text...
+parseFloat() // find float from text...
