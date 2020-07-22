9a3a1d19eac11d393886a849af39dabae97857f4
meSajied <sajiedshahyousuf@gmail.com>
2020-07-22 21:19:19 +0600

    fix: key issue...

--- event/key.js
+++ event/key.js
@@ -11,7 +11,29 @@ function processKeyUp(evt) {
   keys.innerHTML += "keyup: " + evt.key + " code: " + evt.keyCode + "<br>";
 }
 
+/* or we could have written:
+   window.addEventListener('keypress', processKeyPress);
+*/
+
 function processKeyDown(evt) {
   var keys = document.querySelector('#keys');
-  keys.innerHTML += "keydown: " + evt.key + " code: " + evt.keyCode + "<br>";
+  
+   keys.innerHTML += "keypress: " + evt.key + 
+                   " code: " + evt.keyCode + " Modifiers : ";
+
+  var modifiers = "";
+  
+  if(event.shiftKey)
+    modifiers += "SHIFT ";
+
+  if(event.altKey)
+    modifiers += "ALT ";
+
+  if(event.ctrlKey)
+    modifiers += "CTRL ";
+
+  if(modifiers === "")
+    modifiers = "NONE";
+  
+  keys.innerHTML += modifiers + "<br>";
 }
